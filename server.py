7791066726b70761d05c575d580077665b8eb818
Valentin349 <54153718+Valentin349@users.noreply.github.com>
2019-10-14 18:10:44 +0100

    Update server.py
    -Made object oriented
    -Added collision detection (collision effects still not working)

--- server.py
+++ server.py
@@ -4,88 +4,127 @@ from setting import *
 import package
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-IP = socket.gethostbyname(socket.gethostname())
-PORT = 5555
+class Server:
+    # initial dict to save starting settings
+    key = {
+        "x": 400,
+        "y": 400,
 
-# try to bind a socket to the ip and port
-try:
-    sock.bind((IP,PORT))
-except socket.error as error:
-    print(str(error))
+        "velX": 0,
+        "velY": 0,
 
-print("binding successfull")
-# allow number of conns
-sock.listen(5)
+        "accX": 0,
+        "accY": 0,
 
-# intial dict to save starting settings
-key = {
-    "x": 400,
-    "y": 400,
-    "velx": 0,
-    "vely": 0,
+        "player": 0,
+        "lifes": 3,
+        "colour": BLUE
+    }
 
-    "player": 0,
-    "lifes": 3,
-    "colour": BLUE
-}
+    key_2 = {
+        "x": 600,
+        "y": 600,
 
-key_2 = {
-    "x": 600,
-    "y": 600,
-    "velx": 0,
-    "vely": 0,
+        "velX": 0,
+        "velY": 0,
+
+        "accX": 0,
+        "accY": 0,
+
+        "player": 1,
+        "lifes": 3,
+        "colour": WHITE
+
+    }
+
+    keys_players = [key, key_2]
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.IP = socket.gethostbyname(socket.gethostname())
+        self.PORT = 5555
 
 
-    "player": 1,
-    "lifes": 3,
-    "colour": WHITE
-
-}
-
-keys_players = [key, key_2]
-
-def threaded_client(conn, player):
-
-    data_s = package.pack(keys_players[player])
-    conn.send(data_s)
-    reply = ""
-    while 1:
         try:
-            try:
-                data_c = package.unpack(conn.recv(2048))
-            except:
-                break
-
-            keys_players[player] = data_c
-
-            if not data_c:
-                # if no data is received assume disconnected
-                print("Disconnected")
-                break
-            else:
-                if player == 1:
-                    reply = keys_players[0]
-                else:
-                    reply = keys_players[1]
-
-                data_s = package.pack(reply)
-                conn.send(data_s)
+            self.sock.bind((self.IP, self.PORT))
         except socket.error as error:
-            print(error)
-            break
+            print(str(error))
 
-    print("lost connection")
-    conn.close()
+        print("binding successfull")
+        self.connect()
 
-currentPlayer = 0
-while 1:
-    # accept connections from clients
-    conn, addr = sock.accept()
-    print(addr, "has connected")
+    def connect(self):
+        self.sock.listen(5)
 
-    # set up a thread
-    threading._start_new_thread(threaded_client, (conn, currentPlayer))
-    currentPlayer += 1
+        currentPlayer = 0
+        while 1:
+            # accept connections from clients
+            conn, addr = self.sock.accept()
+            print(addr, "has connected")
+
+            # set up a thread
+            threading._start_new_thread(self.threadedClient, (conn, currentPlayer))
+            currentPlayer += 1
+
+    def threadedClient(self, conn, player):
+        data_s = package.pack(self.keys_players[player])
+        conn.send(data_s)
+        reply = ""
+        while 1:
+            try:
+                try:
+                    data_c = package.unpack(conn.recv(2048))
+                except:
+                    break
+
+                self.keys_players[player] = data_c
+                self.collision(player)
+
+                if not data_c:
+                    # if no data is received assume disconnected
+                    print("Disconnected")
+                    break
+                else:
+                    if player == 1:
+                        reply = self.keys_players[0]
+                    else:
+                        reply = self.keys_players[1]
 
 
+                    data_s = package.pack(reply)
+                    conn.send(data_s)
+            except socket.error as error:
+                print(error)
+                break
+
+        print("lost connection")
+        conn.close()
+
+    def collision(self,player):
+        collided = False
+        if (self.keys_players[player]["x"] < self.keys_players[1-player]["x"] + 25 and
+            self.keys_players[player]["x"] + 25 > self.keys_players[1-player]["x"] and
+            self.keys_players[player]["y"] < self.keys_players[1-player]["y"] + 25 and
+            self.keys_players[player]["y"] + 25 > self.keys_players[1-player]["x"]):
+            collided = True
+
+        posDiff = [self.keys_players[1-player]["x"] - self.keys_players[player]["x"], self.keys_players[1-player]["y"] - self.keys_players[player]["y"]]
+        velDiff = [self.keys_players[1-player]["velX"] - self.keys_players[player]["velX"], self.keys_players[1-player]["velY"] - self.keys_players[player]["velY"]]
+        impact = posDiff[0]*velDiff[0] + posDiff[1]*velDiff[1]
+
+        posUnitX = posDiff[0] / (posDiff[0]**2 + posDiff[1]**2)
+        posUnitY = posDiff[1] / (posDiff[0] ** 2 + posDiff[1] ** 2)
+        impulseX = impact*posUnitX
+        impulseY = impact*posUnitY
+
+        if collided:
+            print("collision")
+            self.keys_players[1-player]["velX"] += self.keys_players[1-player]["velX"] - impulseX
+            self.keys_players[1-player]["velY"] += self.keys_players[1-player]["velY"] - impulseY
+
+            #self.keys_players[1-player]["x"] += self.keys_players[1-player]["velX"] + 0.5 * \
+                                                #self.keys_players[1-player]["accX"]
+            #self.keys_players[1-player]["y"] += self.keys_players[1-player]["velY"] + 0.5 * \
+                                                #self.keys_players[1-player]["accY"]
+
+
+s = Server()
