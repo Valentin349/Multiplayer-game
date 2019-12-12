4d28e1a926fccaea23fd34417f59ff35d5e7a329
Valentin349 <54153718+Valentin349@users.noreply.github.com>
2019-12-12 21:52:34 +0000

    Update Objects.py
    added ablilityBlock class

--- game v2/Objects.py
+++ game v2/Objects.py
@@ -7,6 +7,29 @@ class Platform(pg.sprite.Sprite):
         self.x = x
         self.y = y
         self.image = pg.Surface((w, h))
-        self.image.fill(RED)
+        self.image.fill(PURPLE)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+
+class AbilityBlock(pg.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        pg.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
+        self.image = pg.Surface((w, h))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def update(self, data):
+        if data["ability"] is None:
+            self.kill()
+        else:
+            self.rect.center = (data["ability"]["x"], data["ability"]["y"])
+
+            if data["ability"]["Type"] == "utility":
+                self.image.fill(GREEN)
+            elif data["ability"]["Type"] == "defence":
+                self.image.fill(BLUE)
+            else:
+                self.image.fill(RED)
