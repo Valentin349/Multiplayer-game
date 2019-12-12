7717f85c0e8cff839e17e43e68d4915ef9a71d1b
Valentin349 <54153718+Valentin349@users.noreply.github.com>
2019-12-12 21:51:18 +0000

    Update Settings.py
    added new colours

--- game v2/Settings.py
+++ game v2/Settings.py
@@ -4,10 +4,12 @@ HEIGHT = 800
 FPS = 120
 
 # colours
-RED = (204, 0, 0)
+RED = (240, 50, 50)
 BLUE = (0, 102, 204)
 BLACK = (20, 20, 20)
 WHITE = (249, 249, 249)
+GREEN = (7, 171, 53)
+PURPLE = (49, 11, 92)
 
 # player properties
 ACCELERATION = 0.00001
